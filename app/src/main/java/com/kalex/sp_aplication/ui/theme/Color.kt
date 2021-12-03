1f3f55ff712139277008e8ba9d884d76d1ee041d
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-12-02 19:00:43 -0500

    first commit - interfaz sing in al 99%

---
+++ app/src/main/java/com/kalex/sp_aplication/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.kalex.sp_aplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val spcolor = Color(0xFFC8150D)
+val blanco = Color(0xFFFFFFFF)
+val gris = Color(0xFF7B7676)
---
+++ app/src/main/java/com/kalex/sp_aplication/ui/theme/Type.kt
@@ -0,0 +1,19 @@
+package com.kalex.sp_aplication.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.resolveDefaults
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+
+    )
+
+)
